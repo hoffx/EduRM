d93dd812c6f0640982ee9c707b39c410fad48c4e
veecue <veecue@ventos.tk>
2018-03-12 18:27:24 +0100

    finished script

--- script/script.go
+++ script/script.go
@@ -1,25 +1,31 @@
 package script
 
 import (
+	"bufio"
 	"errors"
+	"io"
+	"os"
 	"regexp"
 	"strconv"
 )
 
-var InstructionRegex = regexp.MustCompile(`^([0-9]+)[:\s]+([A-Z]+)\s+([0-9]+)`)
+var instructionRegex = regexp.MustCompile(`^([0-9]+)[:\s]+([A-Z]+)\s+(-{0,1}[0-9]+)`)
 
+// Script represents a script written in assembly ready to be interpreted
 type Script struct {
 	Name         string
 	Path         string
 	Instructions map[int]Instruction
 }
 
+// Instruction represents a single assembly instruction
 type Instruction struct {
 	Number     int
 	Identifier string
 	Parameters []int
 }
 
+// NewScript creates a Script by name
 func NewScript(name string) *Script {
 	return &Script{
 		Name:         name,
@@ -27,15 +33,27 @@ func NewScript(name string) *Script {
 	}
 }
 
+// AppendInstruction adds a given Instruction to the Script
 func (s *Script) AppendInstruction(instruction Instruction) {
 	s.Instructions[instruction.Number] = instruction
 }
 
+// ParseInstruction adds an Instruction given as string to the Script
+func (s *Script) ParseInstruction(instruction string) error {
+	i, err := ParseInstruction(instruction)
+	if err != nil {
+		return err
+	}
+	s.AppendInstruction(i)
+	return nil
+}
+
+// ParseInstruction parses the string representation of an Instruction and returns the Instruction
 func ParseInstruction(instruction string) (Instruction, error) {
-	if !InstructionRegex.MatchString(instruction) {
+	if !instructionRegex.MatchString(instruction) {
 		return Instruction{}, errors.New("no match")
 	}
-	s := InstructionRegex.FindStringSubmatch(instruction)
+	s := instructionRegex.FindStringSubmatch(instruction)
 	if s == nil || len(s) != 4 {
 		return Instruction{}, errors.New("error while matching")
 	}
@@ -46,4 +64,29 @@ func ParseInstruction(instruction string) (Instruction, error) {
 		Identifier: s[2],
 		Parameters: []int{param},
 	}, nil
+}
+
+// ParseFile parses the file in the given location and returns it's contents as a ready assembly script
+func ParseFile(path string) (*Script, error) {
+	f, err := os.Open(path)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	s := NewScript(f.Name())
+	s.Path = path
+	rd := bufio.NewReader(f)
+	for {
+		l, _, err := rd.ReadLine()
+		if err == io.EOF && len(l) == 0 {
+			break
+		} else if err != nil && err != io.EOF {
+			return nil, err
+		}
+		err = s.ParseInstruction(string(l))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
 }
--- script/script_test.go
+++ script/script_test.go
@@ -8,3 +8,7 @@ import (
 func TestParseInstruction(t *testing.T) {
 	fmt.Println(ParseInstruction("12: MOV 1"))
 }
+
+func TestParseFile(t *testing.T) {
+	fmt.Println(ParseFile("test.txt"))
+}
